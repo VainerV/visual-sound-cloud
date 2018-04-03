7b7eed2fe0613a2ab153cc3f66501e36587ff1f0
VainerV <vainerv@yahoo.com>
2018-04-02 22:28:22 -0700

    Code added to JS, HTML and Css files

--- indexajax.js
+++ indexajax.js
@@ -1,14 +1,30 @@
-const NAPSTER_BASE_URL = "https://api.napster.com/v2.2/";
-const NAPSTER_QUERY= {
-      apikey: "MmRiMTFlMmMtMWI4Yy00YjdiLWFiN2MtNDI4Y2UxMjllMDUx",
-      
-    }
+const NAPSTER_BASE_URL = "https://api.napster.com/v2.2/search";
+const NAPSTER_QUERY = "MmRiMTFlMmMtMWI4Yy00YjdiLWFiN2MtNDI4Y2UxMjllMDUx"
+const showErr = "Loading Error";
+  
 
-    function getDataFromApi(searchTerm, callback) {
+$('.js-search-form').on('submit', function(event){
+        event.preventDefault();
+        let searchRequest  = $(".search-text-field").val();
+        let type  = $("#type-select").val();
+        getDataFromApi(searchRequest, type, function (data){
+            console.log(searchRequest);
+            console.log(type);
+        result = data;
+        console.log(result);
+       
+       } )
+      });
+
+    function getDataFromApi(searchTerm, type, callback) {
         const query = {
-          q: searchTerm,
-          key: NAPSTER_QUERY,
+           query: searchTerm,
+           apikey: NAPSTER_QUERY,
+           type: type,
         }
+        
         $.getJSON(NAPSTER_BASE_URL, query, callback)
         .fail(showErr);
       }
+
+      
